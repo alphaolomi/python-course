61895040cff38f411cecb0ef10aef5e8e3595438
Alpha Olomi <alphaolomi@gmail.com>
2020-04-15 19:09:01 +0300

    Update 03_lists.py

--- 03_lists.py
+++ 03_lists.py
@@ -1,7 +1,17 @@
-# A List is a collection which is ordered and changeable. Allows duplicate members.
+'''
+A List is a collection which is ordered and changeable.
+Allows duplicate members.
+'''
 
 # Create list
 numbers = [1, 2, 3, 4, 5]
+
+a= [2,3, 4] # A mixed list
+b= [2,2,7, 3.5, "Hello"]    # An empty list
+c= []   # A list containing a list
+d= [2,[a,b]]    # Join two lists
+e= a + b    # A list of integers
+
 fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
 
 # Use a constructor
@@ -38,3 +48,20 @@ fruits.sort()
 fruits.sort(reverse=True)
 
 print(fruits)
+
+
+
+# Simple maniplation
+
+x = a[1] # Get 2nd element (0 is first)
+y = b[1:3] # Return a sublist
+z = d[1][0][2] # Nested lists
+b[0] = 42 # Change an element
+
+
+
+
+
+
+
+
